39c211b5797f7e2038c752e3c51fb519892cffd8
Gyudong Han <bmil2011s@naver.com>
2023-07-31 02:08:54 +0900

    RestaurantShare_Project-3 Category CRUD

--- Django/Restaurant_share_Project/RestaurantShare/shareRes/urls.py
+++ Django/Restaurant_share_Project/RestaurantShare/shareRes/urls.py
@@ -4,7 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('restaurantDetail/', views.restaurantDetail),
-    path('restaurantCreate/', views.restaurantCreate),
-    path('categoryCreate/', views.categoryCreate),
+    path('restaurantDetail/', views.restaurantDetail, name='resDetailPage'),
+    path('restaurantCreate/', views.restaurantCreate, name='resCreatePage'),
+    path('categoryCreate/', views.categoryCreate, name='cateCreatePage'),
+    path('categoryCreate/create', views.Create_category, name='cateCreate'),
+    path('categoryCreate/delete', views.Delete_category, name='cateDelete'),
 ]
--- Django/Restaurant_share_Project/RestaurantShare/shareRes/views.py
+++ Django/Restaurant_share_Project/RestaurantShare/shareRes/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import *
 
 # Create your views here.
 def index(request):
-    return render(request, 'shareRes/index.html')
+    categories = Category.objects.all()
+    content = {'categories' : categories}
+    return render(request, 'shareRes/index.html', content)
 
 def restaurantDetail(request):
     return render(request, 'shareRes/restaurantDetail.html')
@@ -12,4 +16,18 @@ def restaurantCreate(request):
     return render(request, 'shareRes/restaurantCreate.html')
 
 def categoryCreate(request):
-    return render(request, 'shareRes/categoryCreate.html')
+    categories = Category.objects.all()
+    content = {'categories':categories}
+    return render(request, 'shareRes/categoryCreate.html', content)
+
+def Create_category(request):
+    category_name = request.POST['categoryName']
+    new_category = Category(category_name = category_name)
+    new_category.save()
+    return HttpResponseRedirect(reverse('index'))
+
+def Delete_category(request):
+    category_id = request.POST['categoryId']
+    delete_category = Category.objects.get(id = category_id)
+    delete_category.delete()
+    return HttpResponseRedirect(reverse('cateCreatePage'))
