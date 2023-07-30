844879e5700731113701d7f0e7b79e7966485cb5
Gyudong Han <bmil2011s@naver.com>
2023-07-30 21:11:46 +0900

    RestaurantShare_Project-2 Templates

---
+++ Django/Restaurant_share_Project/RestaurantShare/sendEmail/urls.py
@@ -0,0 +1,6 @@
+# sendEmail > urls.py
+from django.urls import path, include
+from . import views
+urlpatterns = [
+    path('send/', views.sendEmail)
+]
--- Django/Restaurant_share_Project/RestaurantShare/shareRes/views.py
+++ Django/Restaurant_share_Project/RestaurantShare/shareRes/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return render(request, 'shareRes/index.html')
+
+def restaurantDetail(request):
+    return render(request, 'shareRes/restaurantDetail.html')
+
+def restaurantCreate(request):
+    return render(request, 'shareRes/restaurantCreate.html')
+
+def categoryCreate(request):
+    return render(request, 'shareRes/categoryCreate.html')
